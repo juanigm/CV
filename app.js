1c2b67faa4a25836519ad7ef6958fb4fabeaa5fe
juani <juanigm99@gmail.com>
2022-02-24 18:41:27 -0300

    Terminada la página! Listas las correciones, por ahi falten algunas cosas del responsive. Listo el nav responsive.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 addEventListener ('DOMContentLoaded', () => {
     const nav__btn = document.querySelector('.nav__btn');
-    if(nav__btn){
+    const nav__items = document.querySelectorAll('.resp');
+    if(nav__btn && nav__items){
+        const nav__ul = document.querySelector('.nav__ul-responsive');
         nav__btn.addEventListener('click', () => {
-            const nav__ul = document.querySelector('.nav__ul-responsive');
             nav__ul.classList.toggle('show');
         });
+
+        for(var i = 0; i < nav__items.length; i++){
+            nav__items[i].addEventListener('click', () =>{
+                nav__ul.classList.toggle('show');
+            });
+        }
     }
     else{
         console.log("Error");
     }
 });
-
-
